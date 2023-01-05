24053d45a71ef637aa9c90a373a377723908c24f
Jules Ferlin <jules.ferlin@gmail.com>
2023-01-05 22:03:39 +0100

    changement d'indentation et de commentaires

--- main.c
+++ main.c
@@ -69,11 +69,11 @@ struct poste_peage *postes_peage;
 // Tableau de taux de paiement par véhicule
 int montants_classe[5] = {20, 30, 40, 50, 10};
 
-const int TEMPS_ATTENTE[5] = {3, 5, 10, 15, 2}; // Temps d'attente en secondes pour chaque classe de véhicule (voitures, motos, deux-essieux, trois-essieux)
+// Temps d'attente en secondes pour chaque classe de véhicule (voitures, motos, deux-essieux, trois-essieux)
+const int TEMPS_ATTENTE[5] = {3, 5, 10, 15, 2};
 
 // Variables globales pour tout le programme.
 // Elles sont initialisées modifiées en fonction des arguments passés au programme
-
 int nb_vehicules = 100;             // Nombre de véhicules créés
 int nb_postes_peage = 5;            // Nombre de postes de péage
 int voie_telepeage = 0;             // S'il y a une voie de télépéage (1) ou non (0)
@@ -117,7 +117,7 @@ struct vehicule creation_vehicule(int id) {
         case 0:
         case 1:
         case 2:
-        case 3:
+        case 3:     // Tous les choix ont été laissés pour plus de lisibilité
         case 4:
         case 5:
         case 6:
@@ -126,7 +126,6 @@ struct vehicule creation_vehicule(int id) {
         default:
             mode_paiement = ESPECES;
             break;
-
     }
     v.mode_paiement = mode_paiement;
 
@@ -213,8 +212,6 @@ void* simulate_vehicule(void* arg) {
     printf(ANSI_COLOR_RED"Vehicule entrant %d: classe %s, mode de paiement %s, poste de peage %d, nombre d'occupants %d\n"ANSI_COLOR_RESET, v->id, classes_vehicule_str[v->classe], mode_paiement_str[v->mode_paiement], poste_peage_id, v->nb_occupant);
     ajout_queue(&postes_peage[poste_peage_id], *v);
 
-
-
     // Attendre au poste de péage
     // l'attente dépend de la classe du véhicule et de la voie de télépéage (s'il y en a une)
     sleep(1);
@@ -254,8 +251,13 @@ int init_argument(int argc, char* argv[]) {
     // Initialise les variables globales
     switch (argc) {
         case 1:
-            printf("Possibilite d'utilisation: ./%s <nb_vehicules> <nb_postes_peage> <voie_telepeage> <voie_covoiturage> <temps_entre_vehicule>\n", argv[0]);
-            printf("Valeur par defaut\n");
+            printf("Possibilit%c d'utilisation: ./%s "
+                   "<nb_vehicules> "
+                   "<nb_postes_peage> "
+                   "<voie_telepeage> "
+                   "<voie_covoiturage> "
+                   "<temps_entre_vehicule>\n", 130, argv[0]);
+            printf("Valeur par d%cfaut\n", 130);
             break;
         case 2:
             nb_vehicules = atoi(argv[1]);
@@ -269,7 +271,7 @@ int init_argument(int argc, char* argv[]) {
             nb_postes_peage = atoi(argv[2]);
             voie_telepeage = atoi(argv[3]);
             if (voie_telepeage != 0 && voie_telepeage != 1) {
-                printf("Erreur: la voie de telepeage doit etre 0 ou 1\n");
+                printf("Erreur: presence d'une voie de t%cl%cp%cage 0 (Non) ou 1 (oui)\n", 130, 130, 130);
                 return 1;
             }
             break;
@@ -278,7 +280,7 @@ int init_argument(int argc, char* argv[]) {
             nb_postes_peage = atoi(argv[2]);
             voie_telepeage = atoi(argv[3]);
             if (voie_telepeage != 0 && voie_telepeage != 1) {
-                printf("Erreur: presence d'une voie de telepeage 0 (Non) ou 1 (oui)\n");
+                printf("Erreur: presence d'une voie de t%cl%cp%cage 0 (Non) ou 1 (oui)\n", 130, 130, 130);
                 return 1;
             }
             voie_covoiturage = atoi(argv[4]);
@@ -292,28 +294,29 @@ int init_argument(int argc, char* argv[]) {
             nb_postes_peage = atoi(argv[2]);
             voie_telepeage = atoi(argv[3]);
             if (voie_telepeage != 0 && voie_telepeage != 1) {
-                printf("Erreur: la voie de telepeage doit etre 0 ou 1\n");
+                printf("Erreur: presence d'une voie de t%cl%cp%cage 0 (Non) ou 1 (oui)\n", 130, 130, 130);
                 return 1;
             }
             voie_covoiturage = atoi(argv[4]);
             if (voie_covoiturage != 0 && voie_covoiturage != 1) {
-                printf("Erreur: la voie de covoiturage doit etre 0 ou 1\n");
+                printf("Erreur: presence d'une voie de covoiturage 0 (Non) ou 1 (oui)\n");
                 return 1;
             }
             temps_entre_vehicules = atoi(argv[5]);
             if (temps_entre_vehicules <= 0) {
-                printf("Erreur: le temps entre les vehicules doit etre positif et non nul\n");
+                printf("Erreur: le temps entre les v%chicules doit %ctre positif et non nul\n", 130, 136);
                 return 1;
             }
             if (temps_entre_vehicules < 120) {
-                printf("Attention: le temps entre les vehicules est tres court, les resultats seront errones\n");
+                printf("Attention: le temps entre les v%chicules est tres court, "
+                       "les r%csultats seront erron%cs\n", 130, 130, 130);
             }
             if (temps_entre_vehicules > 2000) {
-                printf("Attention: le temps entre les vehicules est tres long\n");
+                printf("Attention: le temps entre les v%chicules est tres long\n", 130);
             }
             break;
         default:
-            printf("Erreur: trop d'arguments, valeurs par defaut\n");
+            printf("Erreur: trop d'arguments, valeurs par d%cfaut\n", 130);
             break;
     }
     return 0;
@@ -328,12 +331,18 @@ int main(int argc, char* argv[]) {
     }
 
     // Affiche les paramètres
-    printf("Parametres:\n"
-           "\t- nb_vehicules = %d\n"
-           "\t- nb_postes_peage = %d\n"
-           "\t- Voie de telepeage = %d\n"
+    printf("Param%ctres:\n"
+           "\t- Nombre de v%chicules = %d\n"
+           "\t- Nombre de postes de p%cage = %d\n"
+           "\t- Voie de t%cl%cp%cage = %d\n"
            "\t- Voie de covoiturage = %d\n"
-           "\t- Temps entre les vehicules (en second) = %.3f\n", nb_vehicules, nb_postes_peage, voie_telepeage, voie_covoiturage, temps_entre_vehicules/1000.0);
+           "\t- Temps entre les v%chicules (en second) = %.3f\n",
+           136,
+           130, nb_vehicules,
+           130, nb_postes_peage,
+           130, 130, 130, voie_telepeage,
+           voie_covoiturage,
+           130, temps_entre_vehicules/1000.0);
     printf("\n\n ========================== Debut du programme (dans 2 sec) ========================== \n\n");
     sleep(2);
 
@@ -360,18 +369,22 @@ int main(int argc, char* argv[]) {
     printf("\n\n ========================== Fin du programme ========================== \n\n");
     // Afficher le total de taxes collectées par chaque poste de péage
     for (int i = 0; i < nb_postes_peage; i++) {
-        printf("Total collecte au poste de peage %d: %d\n", postes_peage[i].id, postes_peage[i].total_collectee);
+        printf("Total collecte au poste de p%cage %d: %d\n", 130, postes_peage[i].id, postes_peage[i].total_collectee);
     }
     printf("\n");
 
-    // Afficher le nombre de véhicules qui ont utilisé le télépéage et ceux qui n'ont pas utilisé le télépéage si la voie de télépéage est définie
+    // Afficher le nombre de véhicules qui ont utilisé le télépéage
+    // et ceux qui n'ont pas utilisé le télépéage si la voie de télépéage est définie
     if (voie_telepeage == 1) {
-        printf("Nombre de vehicules ayant utilise la voie de telepeage: %d\n", nb_telepeage);
-        printf("Nombre de vehicules n'ayant pas utilise la voie de telepeage: %d\n", nb_sans_telepeage);
+        printf("Nombre de v%chicules ayant utilise la voie de t%cl%cp%cage: %d\n", 130, 130, 130, 130, nb_telepeage);
+        printf("Nombre de v%chicules n'ayant pas utilise la voie de t%cl%cp%cage: %d\n", 130, 130, 130, 130, nb_sans_telepeage);
     }
+
+    // Afficher le nombre de véhicules qui ont utilisé le covoiturage
+    // et ceux qui n'ont pas utilisé le covoiturage si la voie de covoiturage est définie
     if (voie_covoiturage == 1) {
-        printf("Nombre de vehicules ayant utilise la voie de covoiturage: %d\n", nb_covoiturage);
-        printf("Nombre de vehicules n'ayant pas utilise la voie de covoiturage: %d\n", nb_sans_covoiturage);
+        printf("Nombre de v%chicules ayant utilise la voie de covoiturage: %d\n", 130, nb_covoiturage);
+        printf("Nombre de v%chicules n'ayant pas utilise la voie de covoiturage: %d\n", 130, nb_sans_covoiturage);
     }
 
     // Libérer la mémoire
